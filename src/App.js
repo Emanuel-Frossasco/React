52b47bba52e121ed27dbe7ee614263d0fd025b42
emanuel frossasco <manufro@hotmail.com>
2022-11-30 19:32:50 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BasicExample from './components/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <BasicExample/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
